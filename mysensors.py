34227ebfac87c491010aae53505f42023c2ac037
zeit0dn1 <keith@timeghost.com>
2023-02-06 14:30:30 -0600

    Added comment

--- mysensors.py
+++ mysensors.py
@@ -1,8 +1,9 @@
+#fine tune your sensor offsets below
 def load():
     global OFFSETS
 OFFSETS = {
-"287dd449f6913cb8" : "10",
-"28bd8949f6093ceb" : "20",
-"28547149f6d63c8c" : "-10",
+"287dd449f6913cb8" : "0.0",
+"28bd8949f6093ceb" : "0.0",
+"28547149f6d63c8c" : "-0.5",
 "2824ec49f6b73cd1" : "0"
 }
